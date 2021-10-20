32dd1c6a73d8ce48dd4a9ec277cf753330ab67ff
Saull Brandão <saull@outlook.com>
2021-10-20 11:10:56 -0300

    feat: create form component to send messages

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -41,6 +41,7 @@ export function AuthProvider({ children }: AuthProvider) {
     const { token, user } = response.data
 
     localStorage.setItem('@dowhile:token', token)
+    api.defaults.headers.common.authorization = `Bearer ${token}`
 
     setUser(user)
   }
